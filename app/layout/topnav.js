e1a23c009ee139ca104179f78a308bdf8deea614
Allen Buckley <allensb@gmail.com>
2014-08-03 21:39:38 -0400

    Initial Commit

---
+++ app/layout/topnav.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    'use strict';
+
+    var controllerId = 'topnav';
+    angular.module('app').controller(controllerId,
+        ['$scope', topnav]);
+
+    function topnav($scope) {
+        $('.mainbar').css("margin-left", "0px");
+
+        $scope.toggleDrawer = function () {
+            if ($('.sidebar').hasClass('hide-drawer')) {
+                $('.sidebar').removeClass('hide-drawer');
+                $('.mainbar').css("margin-left", "230px");
+            } else {
+                $('.sidebar').addClass('hide-drawer');
+                $('.mainbar').css("margin-left", "0px");
+            }
+        }
+    };
+})();
